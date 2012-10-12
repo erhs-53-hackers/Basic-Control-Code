acdbe35bc6e76fe3fd3fea1062dc807eab74a366
Adam Claassen <crazyninjamonkey4@gamil.com>
2012-10-11 20:27:48 -0400

    First commit, both tank and arcade drive projects for a standard 4 motor robot

---
+++ TankDrive/DemoBot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.*;
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends IterativeRobot {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    Joystick stick1, stick2;
+    RobotDrive drive;
+    public void robotInit() {
+        drive = new RobotDrive(1,2,3,4);
+        stick1 = new Joystick(1);
+        stick2 = new Joystick(2);
+        
+        
+    }
+    
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        drive.tankDrive(stick1, stick2);
+    }
+    
+}
